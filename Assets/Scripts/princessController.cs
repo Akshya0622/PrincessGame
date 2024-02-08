08563dd0d16cd24e340af1ddabec19675ed2052b
Akshya0622 <akshya.sri@hotmail.com>
2024-02-07 20:10:31 -0800

    start

---
+++ Assets/Scripts/princessController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class princessController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    float speed = 3f;
+    Rigidbody2D rigidbody2d;
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position); // move player
+    }
+}
