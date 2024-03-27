452ac67146137cd3bce977f52bca66606f548d09
Akshya0622 <akshya.sri@hotmail.com>
2024-03-26 22:34:54 -0700

    yup

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,4 +8,19 @@ public class Tile : MonoBehaviour
     public Tile[] downNeighbors;
     public Tile[] rightNeighbors;
     public Tile[] leftNeighbors;
+    public Tile[] possibilities;
+    public Dictionary <int, Tile> MyNeighbors;
+
+    public void Start()
+    {
+        int entropy = possibilities.Length;
+    }
+    public void addNeighbors(int direction, Tile neigh)
+    {
+        MyNeighbors[direction] = neigh;
+    }
+    public Tile getNeighbors(int direction)
+    {
+        return MyNeighbors[direction];
+    }
 }
--- Assets/Scripts/mapGenerator.cs
+++ Assets/Scripts/mapGenerator.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = System.Random;
 
 public class mapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int sizeX = 50;
+    public int sizeY = 50;
+    public void makeGrid()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+   
+
 }
