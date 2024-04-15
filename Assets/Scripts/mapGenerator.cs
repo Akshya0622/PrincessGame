b310617b8271b74b8ee92dc1b0dad82ab29242d9
Akshya0622 <akshya.sri@hotmail.com>
2024-04-15 15:38:29 -0700

    :(

--- Assets/Scripts/mapGenerator.cs
+++ Assets/Scripts/mapGenerator.cs
@@ -7,21 +7,39 @@ using Random = UnityEngine.Random;
 
 public class mapGenerator : MonoBehaviour
 {
-    public int sizeX = 50;
-    public int sizeY = 50;
+    public int sizeX = 10;
+    public int sizeY = 10;
     public Tile[] tilePrefabs;
+    Dictionary<Tile, int> tileWeightss;
+    public int[] weightValues;
     public void Start()
     {
+        tileWeightss = populateTileWeights();
         makeGrid();
+
+    }
+    Dictionary<Tile,int> populateTileWeights()
+    {
+        Dictionary<Tile, int> tileWeights = new Dictionary<Tile, int>();
+        for (int i = 0; i< tilePrefabs.Length; i++)
+        {
+            tileWeights.Add(tilePrefabs[i], weightValues[i]);
+        }
+        return tileWeights;
     }
     public void makeGrid()
     {
         Tile[,] world = new Tile[sizeX, sizeY];
         Vector2Int startPos = new Vector2Int(sizeX / 2, sizeY / 2);
-        generateTiles(world, startPos.x, startPos.y);
+        Tile startTile = tilePrefabs[Random.Range(0, tilePrefabs.Length)];
+        world[startPos.x, startPos.y] = startTile;
+        generateTiles(world, startPos.x, startPos.y, startTile);
     }
-    void generateTiles(Tile[,] world, int x, int y)
+    void generateTiles(Tile[,] world, int x, int y, Tile tilePrfb)
     {
+        
+        Debug.Log("Generating tile at: " + x + ", " + y);
+        Instantiate(tilePrfb, new Vector3(x,y, 0), Quaternion.identity);
         if (x < 0 || x >= sizeX || y < 0 || y >= sizeY)
         {
             return;
@@ -50,16 +68,23 @@ public class mapGenerator : MonoBehaviour
 
             if (newX >= 0 && newX < sizeX && newY >= 0 && newY < sizeY && world[newX, newY] == null)
             {
-                Tile[] possibleNeighbors = getPossibleNeighbors(world, newX, newY, direction); // access from tile
+                Tile[] possibleNeighbors = getPossibleNeighbors(world, x, y, direction); // access from tile
+                Debug.Log("Number of possible neighbors: " + possibleNeighbors.Length + "direction " + direction);
                 possibleNeighbors = weightedSelect(possibleNeighbors); // u need to makesomething to remake the array with weights
 
                 foreach (Tile neighbor in possibleNeighbors)
                 {
+                    Debug.Log("Checking placement for neighbor: " + neighbor.name);
                     if (canBePlaced(world, newX, newY, neighbor))
                     {
+                        Debug.Log("Placing tile at: " + newX + ", " + newY);
                         world[newX, newY] = neighbor;
-                        generateTiles(world, newX, newY);
+                        generateTiles(world, newX, newY, neighbor);
                         break;
+                    }
+                    else
+                    {
+                        Debug.Log("cant be placed");
                     }
                 }
             }
@@ -69,45 +94,125 @@ public class mapGenerator : MonoBehaviour
         {
             if (x < 0 || x >= sizeX || y < 0 || y >= sizeY || world[x, y] != null)
             {
+                Debug.Log("Cannot place tile at: " + x + ", " + y + ". Out of bounds or tile already exists.");
                 return false;
             }
 
             
 
-            bool canConnectUp = canConnectToNeigh(world, x, y + 1, tile.downNeighbors);
-            bool canConnectDown = canConnectToNeigh(world, x, y - 1, tile.upNeighbors);
-            bool canConnectLeft = canConnectToNeigh(world, x-1, y, tile.rightNeighbors);
-            bool canConnectRight = canConnectToNeigh(world, x+1, y, tile.leftNeighbors);
+            bool canConnectUp = canConnectToNeigh(world, x, y + 1, tile.upNeighbors);
+            bool canConnectDown = canConnectToNeigh(world, x, y - 1, tile.downNeighbors);
+            bool canConnectLeft = canConnectToNeigh(world, x-1, y, tile.leftNeighbors);
+            bool canConnectRight = canConnectToNeigh(world, x+1, y, tile.rightNeighbors);
 
-            if(!canConnectUp || !canConnectDown || !canConnectLeft || !canConnectRight)
+            if (canConnectUp == false)
             {
+                Debug.Log("tried but couldnt 1");
+                return false;
+
+            }
+            else if(canConnectDown == false)
+            {
+                Debug.Log("tried but couldnt 2");
                 return false;
             }
-            return true;
+            else if(canConnectLeft == false)
+            {
+                Debug.Log("tried but couldnt 3");
+                return false;
+            }
+            else if(canConnectRight == false)
+            {
+                Debug.Log("tried but couldnt 4");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        
+
         }
         bool canConnectToNeigh(Tile[,] world, int x, int y, Tile[] possibleNeighbors)
         {
-            if( possibleNeighbors == null || possibleNeighbors.Length == 0 )
+           if( possibleNeighbors == null || possibleNeighbors.Length == 0 )
             {
                 return true;
             }
             if(x>= 0 && x<sizeX && y>= 0 && y<sizeY)
             {
+               
                 foreach(Tile neighborPrefab in possibleNeighbors)
                 {
-                    if (world[x,y] != null && world[x,y].gameObject == neighborPrefab)
+                    if (world[x,y] == null ||  world[x,y] == neighborPrefab)
                     {
+                        Debug.Log("yes");
                         return true;
+                        
                     }
                 }
             }
             return false;
         }
 
-        List<Tile> getPossibleNeighbors(Tile [,] world, int x, int y, Vector2Int direction)
+        Tile[]getPossibleNeighbors(Tile [,] world, int x, int y, Vector2Int direction)
         {
             Tile t = world[x, y];
+            if(t == null)
+            {
+                return new Tile[0];
+               
+
+            }
+            Tile tscript = t.GetComponent<Tile>();
+            if(direction == new Vector2Int(0,1))
+            {
+                return tscript.upNeighbors;
+            }
+           else if (direction == new Vector2Int(0, -1))
+            {
+                return tscript.downNeighbors;
+            }
+            else if (direction == new Vector2Int(-1, 0))
+            {
+                return tscript.leftNeighbors;
+            }
+            else if (direction == new Vector2Int(1, 0))
+            {
+                return tscript.rightNeighbors;
+            }
+            else 
+            {
+                return new Tile[0];
+            }
+
         }
+        Tile[] weightedSelect(Tile[] neighbors)
+        {
+           List<Tile> weightedSelect = new List<Tile>();
+            
+           foreach(Tile neighbor in neighbors)
+            {
+                int weight;
+                if(tileWeightss.TryGetValue(neighbor, out weight))
+                {
+                    for(int i = 0; i < weight; i++)
+                    {
+                        weightedSelect.Add(neighbor);
+                    }
+                }
+            }
+            Tile[] shuffle = weightedSelect.ToArray();
+            for (int i = 0; i < shuffle.Length; i++)
+            {
+                int randomIndex = Random.Range(i, shuffle.Length);
+                Tile temp = shuffle[i];
+                shuffle[i] = shuffle[randomIndex];
+                shuffle[randomIndex] = temp;
+            }
+            return shuffle;
+
+        } 
 
 
 
